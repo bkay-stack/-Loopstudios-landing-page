7af349a264ced9ef0c90428bcc235ae62583126e
bkay-stack <kevinbest1900@gmail.com>
2024-05-31 13:04:58 +0100

    loop

--- src/conponet/Nav.jsx
+++ src/conponet/Nav.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-
+import { useState } from "react";
 const Nav = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
   return (
     <nav className="nav-bar">
       <div className="nav-wrap">
         <div className="logo-wrap">
           <h1>loopstudio</h1>
         </div>
-        <div className="off-screen-menu">
+        <div className={`off-screen-menu ${isActive ? "visible" : ""}`}>
           <ul>
             <li>About</li>
             <li>Careers</li>
@@ -17,7 +22,7 @@ const Nav = () => {
           </ul>
         </div>
 
-        <div className="ham-menu">
+        <div className="ham-menu" onClick={toggleMenu}>
           <span></span>
           <span></span>
           <span></span>
