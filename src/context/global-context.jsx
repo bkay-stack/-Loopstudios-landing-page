c444d5c369b3030eca8c4e2639f04a07f4452a17
bkay-stack <kevinbest1900@gmail.com>
2024-07-14 17:49:41 +0100

    Heder

--- src/context/global-context.jsx
+++ src/context/global-context.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 
 export const AppContext = createContext();
 const AppProvider = ({ children }) => {
@@ -8,6 +8,12 @@ const AppProvider = ({ children }) => {
   const toggleMenu = () => {
     setIsActive(!isActive);
   };
+  useEffect(() => {
+    const bodyElement = document.querySelector("body");
+    isActive
+      ? bodyElement.classList.add("active")
+      : bodyElement.classList.remove("active");
+  }, [isActive]);
   return (
     <AppContext.Provider value={{ toggleMenu, isActive }}>
       {children}
